c33572462dbd3cb267dd84d4c8f3de60164d55b2
yvesyang <64040351+531Yvonne@users.noreply.github.com>
2023-06-29 03:35:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,18 +11,3 @@
         document.body.classList.toggle("light-mode");
     })
 })();
-
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function() {
-  // Check if the URL has a hash value
-  if (window.location.hash) {
-    // Get the section ID from the URL hash
-    var sectionId = window.location.hash.substring(1);
-    
-    // Scroll to the section with the corresponding ID
-    var section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView();
-    }
-  }
-});
