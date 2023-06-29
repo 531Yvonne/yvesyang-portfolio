23834c8659c334e882316c1c23234374b40df080
yvesyang <64040351+531Yvonne@users.noreply.github.com>
2023-06-29 03:25:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,12 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// Navigate to specific section based on url tail 
+document.addEventListener('DOMContentLoaded', () => {
+  const tail = window.location.hash.substring(1); // Exclude the "#" character
+  const section = document.getElementById(tail);
+  if (section) {
+    section.scrollIntoView();
+  }
+});
